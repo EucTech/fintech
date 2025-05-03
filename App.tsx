6438ae7f8d55f810a5adb49d91ff1cdbe06e1d9a
EucTech <euccreative@gmail.com>
2025-05-03 16:44:08 -0400

    updated

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import "./global.css";
+import { StatusBar } from "expo-status-bar";
+import RootNavigation from "./src/screens/navigation/RootNavigation";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <RootNavigation />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+});
