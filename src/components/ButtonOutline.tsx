f7f55703e74b077b18dbcc2689fe63b4e9e03392
EucTech <euccreative@gmail.com>
2025-05-06 10:23:20 -0400

    created custom outline button

--- src/components/ButtonOutline.tsx
+++ src/components/ButtonOutline.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { Text, Pressable, View } from 'react-native'
 import React from 'react'
 
-const ButtonOutline = () => {
+interface ButtonOutlineProps {
+  title: string;
+  action?: () => void;
+  children?: React.ReactNode;
+}
+
+const ButtonOutline: React.FC<ButtonOutlineProps> = ({title, action, children}) => {
   return (
-    <View>
-      <Text>ButtonOutline</Text>
-    </View>
+    <Pressable className='border-2 border-neutral-400 rounded-lg justify-center items-center py-4 flex-row space-x-2'
+    onPress={action}
+    >
+      {children && <View>{children}</View>}
+      <Text className='text-neutral-400 font-bold text-lg'>{title}</Text>
+    </Pressable>
   )
 }
 
-export default ButtonOutline
+export default ButtonOutline;
