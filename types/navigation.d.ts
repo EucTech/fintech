c25b231ce766bd957f5ef3ed6f15c483719b4f72
EucTech <euccreative@gmail.com>
2025-05-07 08:26:23 -0400

    addedd types

--- types/navigation.d.ts
+++ types/navigation.d.ts
@@ -1,6 +1,15 @@
-
-
 // Type
+
+type AuthNavigationType = {
+    Welcome: undefined;
+    Login: undefined;
+    Register: undefined;
+    Splash: undefined;
+}
+
+// type TabNavigationType = {
+//     Home: undefined;
+// }
 
 type SplashNavigationType = {
     Welcome: undefined;
