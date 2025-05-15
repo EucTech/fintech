af6d409282bd63764cfcd229c02316272d838ebe
EucTech <euccreative@gmail.com>
2025-05-15 14:46:42 -0400

    update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
 import RootNavigation from "./src/screens/navigation/RootNavigation";
 import useCachedResources from "./hooks/useChachedResources";
+import { useUserStore } from "./store/useUserStore";
 
 const App = () => {
 
   const isLoadingComplete = useCachedResources();
+
+  const { session, user } = useUserStore();
+
+  useEffect(() => console.log(user, session), [session, user]);
 
   if (!isLoadingComplete) {
     return null;
