cf1d9fd97a8d49af7ef9494f38b318731e59b4df
EucTech <euccreative@gmail.com>
2025-05-15 20:18:54 -0400

    implemented the index tab to show all tabs

--- src/screens/navigation/RootNavigation.tsx
+++ src/screens/navigation/RootNavigation.tsx
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
+import { useUserStore } from "@/store/useUserStore";
+import { NavigationContainer } from "@react-navigation/native";
 import {
   TransitionPresets,
   createStackNavigator,
 } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
 import AuthNavigation from "./AuthNavigation";
 import TabNavigation from "./TabNavigation";
 
 const Stack = createStackNavigator();
 
 const RootNavigation = () => {
-  const [session, setSession] = useState(false);
+  const { session  } = useUserStore();
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
           ...TransitionPresets.SlideFromRightIOS,
-          animation: true,
-          gesture: true,
+          // animationEnabled: true,
+          gestureEnabled: true,
           gestureDirection: "horizontal",
         }}
       >
-        {session ? (
+        {session && session.user ? (
           <Stack.Screen name="TabNavigation" component={TabNavigation} />
         ) : (
           <Stack.Screen name="AuthNavigation" component={AuthNavigation} />
