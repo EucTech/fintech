09a36c2e720328274f6d59668e9e24ff220ac641
EucTech <euccreative@gmail.com>
2025-05-17 07:30:28 -0400

    set up the home navigation

--- src/screens/navigation/TabNavigation/HomeNavigation.tsx
+++ src/screens/navigation/TabNavigation/HomeNavigation.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import {
+  TransitionPresets,
+  createStackNavigator,
+} from "@react-navigation/stack";
+import HomeScreen from "../../tabs/home/homeScreen";
+import CoinDetailsScreen from "../../stacks/CoinDetailsScreen";
+
+const Stack = createStackNavigator();
 
 const HomeNavigation = () => {
   return (
-    <View>
-      <Text>HomeNavigation</Text>
-    </View>
-  )
-}
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.SlideFromRightIOS,
+        // animationEnabled: true,
+        gestureEnabled: true,
+        gestureDirection: "horizontal",
+      }}
+    >
+      <Stack.Screen name="HomeS" component={HomeScreen} />
+      <Stack.Screen name="CoinDetails" component={CoinDetailsScreen} />
+    </Stack.Navigator>
+  );
+};
 
-export default HomeNavigation
+export default HomeNavigation;
