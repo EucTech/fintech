5c482e11c86d9fefe8b071772a4f804f217a3a15
EucTech <euccreative@gmail.com>
2025-05-19 20:51:52 -0400

    added image constants

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,9 @@
 import logo from "@/assets/images/logo.png";
+import money_send from "@/assets/images/money-send.png";
+import money_receive from "@/assets/images/money-receive.png";
+import card_add from "@/assets/images/card-add.png";
+import more from "@/assets/images/more.png";
 
 
-export const Images = { logo };
+
+export const Images = { logo, money_send, money_receive, card_add, more };
